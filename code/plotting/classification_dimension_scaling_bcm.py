9c91ef61e0cc3a278818acdc0c20892d297d7845
Fabian Schubert <fj.schubert@freenet.de>
2021-03-30 21:32:29 +0200

    .#

---
+++ code/plotting/classification_dimension_scaling_bcm.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+from stdParams import *
+from src.plottools import gen_mesh_ax
+plt.style.use('mpl_style.mplstyle')
+import os
+
+data = np.load(os.path.join(DATA_DIR,"classification_scaling_dimension_bcm.npz"))
+
+perf = data["perf"]
+dim = data["distract_dimension"]
+s = data["distract_scaling"]
+
+dim_ax = gen_mesh_ax(dim)
+s_ax = gen_mesh_ax(s)
+
+n_sweep = perf.shape[2]
+
+perf_mean = perf.mean(axis=2)
+
+fig, ax = plt.subplots(1,3,gridspec_kw={'width_ratios': [15,15,1]}, figsize=(FIG_WIDTH,FIG_WIDTH*0.45))
+
+pc0 = ax[0].pcolormesh(s_ax,dim_ax,perf_mean[:,:,0].T,rasterized=True,vmin=0.5,vmax=1.)
+pc1 = ax[1].pcolormesh(s_ax,dim_ax,perf_mean[:,:,1].T,rasterized=True,vmin=0.5,vmax=1.)
+
+ax[0].set_xlabel(r'$s$')
+ax[1].set_xlabel(r'$s$')
+
+ax[0].set_ylabel(r'$N_{dist}$')
+ax[1].set_ylabel(r'$N_{dist}$')
+
+#ax[0].set_xlim(right=5.)
+#ax[1].set_xlim(right=5.)
+
+ax[0].set_title("Compartment Model",loc="right")
+ax[1].set_title("Point Model",loc="right")
+
+plt.colorbar(pc0,cax=ax[2])
+
+fig.tight_layout(pad=0.2)
+
+fig.savefig(os.path.join(PLOT_DIR,"classification_dimension_scaling_bcm.pdf"))
+fig.savefig(os.path.join(PLOT_DIR,"classification_dimension_scaling_bcm.png"),dpi=600)
+
+plt.show()
